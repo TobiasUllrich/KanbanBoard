8c7316a4bb6000242764297495c7e9d84ad01e16
Tobias Ullrich <ullrich.tobias@gmx.de>
2023-02-21 11:40:04 +0100

    Hey

--- Board/admin.py
+++ Board/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import List, Ticket, TicketToUser
 
 # Register your models here.
-
 class TicketToUserInline(admin.TabularInline):
-    model = Ticket.ticket_to_user.through #Um das ManyToMany-Field im Admin-Interface darzustellen
-class TicketAdmin(admin.ModelAdmin):
-   fields = ('ticket_description','ticket_title','ticket_duedate','ticket_prio','ticket_created_at','ticket_list') #Diese Felder werden angezeigt
-   inlines = [TicketToUserInline] #Um das ManyToMany-Field im Admin-Interface darzustellen
+    model = Ticket.ticket_to_user.through #To see the ManyToMany-Field in the Admin-Interface
 
+
+class TicketAdmin(admin.ModelAdmin):
+   fields = ('ticket_description','ticket_title','ticket_duedate','ticket_prio','ticket_created_at','ticket_list') #Shown fields
+   inlines = [TicketToUserInline] #To see the ManyToMany-Field in the Admin-Interface
 
 
 admin.site.register(List)
-admin.site.register(Ticket, TicketAdmin) #Es wird eine Ansicht 'TicketAdmin' für das Ticket-Model erschaffen
-admin.site.register(TicketToUser)
+admin.site.register(Ticket, TicketAdmin) #A view 'TicketAdmin' for the model Ticket is created in the Admin-Panel
+admin.site.register(TicketToUser) 
